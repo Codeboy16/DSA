0db4f858621dfd801d78375aa4b152ae8fad9827
CodingBoy16 <rs1644818@gmail.com>
2024-09-22 12:57:14 +0530

    Squre Code TO Print

---
+++ starPattern/square.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int main(){
+    //Printing The square
+    for(int a=0; a<10; a++){
+        for(int b=0; b<10; b++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+
+   return 0;
+}
+
+
+
+/* Output
+
+**********
+**********
+**********
+**********
+**********
+**********
+**********
+**********
+**********
+**********
+
+*/
