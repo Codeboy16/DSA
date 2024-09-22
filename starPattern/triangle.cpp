5250c40d4fbdd98dc9d7c4c71ea77ba24d3df7b8
CodingBoy16 <rs1644818@gmail.com>
2024-09-22 13:08:19 +0530

    Triangle Code

---
+++ starPattern/triangle.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+int main(){
+    //trangle The 
+    for(int a=1; a<10; a++){
+        for(int b=0; b<a; b++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+
+   return 0;
+}
+
+
+
+
+
+
+/* Output
+
+*
+**
+***
+****
+*****
+******
+*******
+********
+*********
+
+*/
